6f636af5444bf69fdb8228663739b5dc0471bc61
Kunal Hande <146926197+Kunalhande@users.noreply.github.com>
2026-02-01 23:56:27 +0530

    Fix formatting in db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Hk@12233",   // your password
+  password: "Hk@12233",  
   database: "nimap_test"
 });
 
